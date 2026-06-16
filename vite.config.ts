4b0d0cdcfa331bcc4f0b14492f53f21e318b2b46
Emmanuel Marcos de Oliveira <oliveirafullstack@gmail.com>
2026-06-16 13:06:33 -0300

    feat: add rollup-plugin-visualizer to analyze bundle size

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
+import { visualizer } from 'rollup-plugin-visualizer'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      open: true,
+      gzipSize: true,
+    }),
+  ],
 })
